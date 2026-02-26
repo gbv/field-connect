38f5df02ab71ef4d8df7943e946f15ed0f84a9ad
tkleinke <thomas.kleinke@gbv.de>
2026-02-26 17:04:08 +0100

    Edit EPSG code error message

--- field_connect_dockwidget.py
+++ field_connect_dockwidget.py
@@ -482,7 +482,7 @@ class FieldConnectDockWidget(QtWidgets.QDockWidget, FORM_CLASS):
             self.mB.pushWarning(
                 self.plugin_name,
                 self.tr(
-                    "Invalid EPSG Code in Field Desktop: {epsgId}. Using QGIS project CRS."
+                    "Invalid EPSG Code in project properties: {epsgId}. Using QGIS project CRS."
                 ).format(epsgId=epsg_id),
             )
 
